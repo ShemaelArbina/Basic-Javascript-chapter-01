89a03bbd5fb8d492bddfc565115879c69122130f
Arbina shemael <67178144+ShemaelArbina@users.noreply.github.com>
2022-04-29 23:49:11 -0400

    First commit

---
+++ plants.js
@@ -0,0 +1,5 @@
+// JavaScript source code
+// define variables containing img src values
+var blanket = "images/blanket.jpg";
+var bluestem = "images/bluestem.jpg";
+var rugosa = "images/rugosa.jpg";
